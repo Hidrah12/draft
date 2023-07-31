9a154f54be043d8fe1f667e474f8cafc15b8c59a
dan-ross <danielvillasantioficial@gmail.com>
2023-07-31 19:55:42 -0400

    config: se configuro tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../src/apps/core/templates/core/*.html",
+    "../src/apps/core/templates/core/components/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
