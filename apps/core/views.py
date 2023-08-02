7acad980701545d0e8fccbd437affcc0abeabb52
dan-ross <danielvillasantioficial@gmail.com>
2023-08-01 23:07:53 -0400

    feat: se creo la logica para el formulario en la vista index

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import TaskForm
+from .models import Task
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    if request.method == 'POST':
+        task_form = TaskForm(requet.POST)
+        if task_form.is_valid():
+            Task.objects.create(
+                user=request.user.username,
+                title=request.POST['title'],
+                description=request.POST['description'],
+                tag=request.POST['tags'],
+                important=request.POST['important']
+            )
+            return redirect('core:index')
+    else:
+        task_form = TaskForm()
+    return render(request, 'core/index.html', {'task_form': task_form})
