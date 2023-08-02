6c7f44d5c33e1d6e71dfc8bcc924961952ec34b2
dan-ross <danielvillasantioficial@gmail.com>
2023-08-01 21:21:37 -0400

    style: se creo el panel para crear tareas

--- static/js/script.js
+++ static/js/script.js
@@ -2,13 +2,35 @@
 let buttonShowSidePanel = document.getElementById('btnShowSidePanel');
 buttonShowSidePanel === null || buttonShowSidePanel === void 0 ? void 0 : buttonShowSidePanel.addEventListener('click', (e) => {
     let sidePanel = document.getElementById('sidePanel');
-    console.log(sidePanel);
     if (sidePanel) {
         if (sidePanel.classList.toString().includes('active-side-panel')) {
             sidePanel.classList.remove('active-side-panel');
         }
         else {
             sidePanel.classList.add('active-side-panel');
+        }
+    }
+    e.preventDefault();
+});
+let btnCreateTask = document.getElementById('btnCreateTask');
+btnCreateTask === null || btnCreateTask === void 0 ? void 0 : btnCreateTask.addEventListener('click', (e) => {
+    let createTaskPanel = document.getElementById('taskPanel');
+    if (createTaskPanel) {
+        if (createTaskPanel.classList.toString().includes('invisible')) {
+            createTaskPanel.classList.replace('invisible', 'visible');
+        }
+        else {
+            createTaskPanel.classList.add('invisible');
+        }
+    }
+    e.preventDefault();
+});
+let btnCloseTaskPanel = document.getElementById('btnCloseTaskPanel');
+btnCloseTaskPanel === null || btnCloseTaskPanel === void 0 ? void 0 : btnCloseTaskPanel.addEventListener('click', e => {
+    let createTaskPanel = document.getElementById('taskPanel');
+    if (createTaskPanel) {
+        if (createTaskPanel.classList.toString().includes('visible')) {
+            createTaskPanel.classList.replace('visible', 'invisible');
         }
     }
     e.preventDefault();
--- static/ts/script.ts
+++ static/ts/script.ts
@@ -10,3 +10,26 @@ buttonShowSidePanel?.addEventListener('click', (e) => {
 }
     e.preventDefault()
 })
+let btnCreateTask = document.getElementById('btnCreateTask')
+btnCreateTask?.addEventListener('click', (e) => {
+    let createTaskPanel = document.getElementById('taskPanel') as HTMLDivElement | null
+    if (createTaskPanel) {
+        if (createTaskPanel.classList.toString().includes('invisible')) {
+            createTaskPanel.classList.replace('invisible', 'visible')
+        } else {
+            createTaskPanel.classList.add('invisible')
+        }
+    }
+    e.preventDefault()
+})
+
+let btnCloseTaskPanel = document.getElementById('btnCloseTaskPanel')
+btnCloseTaskPanel?.addEventListener('click', e => {
+    let createTaskPanel = document.getElementById('taskPanel') as HTMLDivElement | null
+    if (createTaskPanel) {
+        if (createTaskPanel.classList.toString().includes('visible')) {
+            createTaskPanel.classList.replace('visible', 'invisible')
+        } 
+    }
+    e.preventDefault()
+})
